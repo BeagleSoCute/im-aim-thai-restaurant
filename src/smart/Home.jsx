a51b73354e10971b549f6878b3244008c86ba278
20201157-TANAW\admin <earthlono123@gmail.com>
2023-04-23 18:29:39 +1200

    Remove unuse code

--- src/smart/Home.jsx
+++ src/smart/Home.jsx
@@ -7,7 +7,7 @@ import StoryTellingSection from "components/home/StoryTellingSection";
 import SpecialMenu from "components/home/SpecialMenu";
 import MenuComponent from "components/menu/MenuComponent";
 import ChefInfo from "components/home/ChefInfo";
-import ReviewSection from "components/home/ReviewSection";
+// import ReviewSection from "components/home/ReviewSection";
 import ContactSection from "components/home/ContactSection";
 import { menuDetails, menuTypes, showMenuTypes } from "data/menu";
 import { strengthData, contactDetails } from "data/common";
